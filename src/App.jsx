a20dc8d189abe4b63b0517ee5b7fff1f61ede334
Sebastian <sebastian.21@outlook.com>
2025-07-11 15:06:53 -0600

    adding web3auth

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ const App = () => {
   useEffect(() => {
     const initWeb3Auth = async () => {
       setInitialLoading(true); // ✅ Always begin in loading state
-      const mobile = isMobileDevice();
+      //const mobile = isMobileDevice();
       try {
         const w3a = new Web3Auth({
           clientId,
@@ -68,6 +68,7 @@ const App = () => {
           walletServicesConfig: {}, // optional services config
         });
 
+        /*
         if (mobile && w3a.cachedAdapter) {
           console.log("Mobile reload detected, clearing session...");
           await safeLogout();
@@ -76,6 +77,7 @@ const App = () => {
           await new Promise((r) => setTimeout(r, 200));
           return;
         }
+          */
 
         await w3a.init(); // always initialize here
         setWeb3Auth(w3a);
