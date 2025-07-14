afe4e09b36715a18dec09643af76d617f36ce3a7
Sebastian <sebastian.21@outlook.com>
2025-07-14 17:16:31 -0600

    adding web3auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Buffer } from "buffer";
+import process from "process";
+window.Buffer = Buffer;
+window.process = process;
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
