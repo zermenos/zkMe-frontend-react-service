ff631f3e3130d176d73e6e460c3d6a6c4b673ba4
Sebastian <sebastian.21@outlook.com>
2025-07-14 23:26:46 -0600

    adding zkme verifier

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <Web3AuthProvider>
-    <StrictMode>
+  <StrictMode>
+    <Web3AuthProvider>
       <App />
-    </StrictMode>
-  </Web3AuthProvider>
+    </Web3AuthProvider>
+  </StrictMode>
 );
