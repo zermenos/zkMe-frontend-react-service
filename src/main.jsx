8a9c49820337e877e97174ddb04f1920a01498aa
Sebastian <sebastian.21@outlook.com>
2025-07-14 23:25:43 -0600

    adding zkme verifier

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Web3AuthProvider } from "@web3auth/modal/react";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Web3AuthProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Web3AuthProvider>
 );
